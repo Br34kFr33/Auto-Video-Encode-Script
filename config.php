4aa966da89876543c1f33c575a132aa6df5722da
Br34kFr33 <33837838+Br34kFr33@users.noreply.github.com>
2019-05-27 21:57:41 -0400

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$tv = '/home/shows/';
-$trash = '/home/Trash/';
-
-//Remove un-needed words from TV Show name
-$tvwatch = array(" ", ".720p", ".1080p", ".internal", ".repack", ".proper", ".hdtv", ".x264", ".web", ".webrip", ".h264", "-strife", "-skgtv", "-lol", "-killers", "-YesTV", "-w4f", "-CRiMSON", "-FLEET", "-ASAP", "-C4TV", "-DEFLATE", "-DIMENSION", "-REWARD", "-DEMAND", "-AVS", "-BATV", "-SVA", "-AVS", "-TBS", "-IMMERSE", "-2HD");
